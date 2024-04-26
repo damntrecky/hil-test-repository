fbc37683750417f749950ee291d6d7ac23443271
Nick Ardecky <nardeck@amazon.com>
2024-04-26 14:12:16 -0700

    Chore - update JUnit dependency in base pom file

--- java-8-test-application/src/test/java/org/example/AppTest.java
+++ java-8-test-application/src/test/java/org/example/AppTest.java
@@ -1,40 +1,13 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-
+    @Test
+    void testApp() {
         App.test();
-        assertTrue( true );
+        assertTrue(true);
     }
 }
